6457c2dd948b2769a7b7d25b55749cec1e7d6ccb
ishaangign <82378234+ishaangign@users.noreply.github.com>
2024-02-02 00:28:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("your number is less than 0.5")
+//     console.log(random);
+
+// }
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!")
+//     console.log(random);
+// }
+
+//const dayofWeek ='monday';
+
+// if (dayofWeek ==='monday'){
+//     console.log("ugh i hate saturdays'!")
+// }
+// else if (dayofWeek ==='saturday'){
+//     console.log("i love em bx")
+
+// }
+// else if (dayofWeek ==='sunday'
+// )
+// {console.log("finally")}
+
+// let numberofEntry = 10;
+
+// if (numberofEntry >=10){
+//     console.log("entry In")
+
+// }
+//{
+// else if(numberofEntry <=10)
+// {console.log("no entry")
+
+
+// const dayofWeek =prompt('ENTER A DAY').toLowerCase();
+
+// if (dayofWeek === 'monday') {
+//     console.log("UGH I HATE MONDAYS")
+// } else if (dayofWeek === 'saturday') {
+//     console.log("pretty close to a fridayyyyy") 
+// } else if (dayofWeek === 'friday') {
+//     console.log("close for weekend")
+// }
+// else {
+//     console.log("fin!ugh")
+// }
+
+// for (let i =1; i<= 10; i++)
+// {
+//     console.log(i);
+// }
+
+// for (let i=100; i>=0; i-=10) {
+// console.log(i)}
+
+
+// const cars = ["abarth", "Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW",]
+    
+//  for (let i= 0; i < cars.length; i++)
+//  {console.log(i, cars[i])
+//  }
+
+// for (let i= 1; i<=10; i++)
+// {
+//     console.log('i' )
+// }
+
+// let input = prompt("hey,TEXT IN THE INPUT COMMAND!");
+// while (true) {
+//     input= prompt(input);
+//     if(input.toLowerCase()==="stop")
+//     break;
+
+// }
+// console.log("ok you win");
+
+// function rant (message)
+// {
+//     console.log(message.toUpperCase());
+//     console.log(message.toUpperCase());
+//     console.log(message.toUpperCase());
+// }
+
+// function returnDay (num){
+//     const days =[null,'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+//     if (num< 1 ||num>7){
+//         return null;
+//     }
+//     else{
+//         return days[num];
+//    
+//     }
+// }
+// function capitalize (word) {
+//     return word[0].toUpperCase()+word.slice(1);
+// }
+
+//const input ={a:1,b:2,c:3};
+//const Output=Object.entities(input).map((key,value)) => ({[key]:value})
+
+//console.log(Output)
+
+
+$ npm install -g qrcode
+
+
+
+
+
+
